7b1e5fdc61784704b9f4d112c0a82fbc6552c4fd
Volha.Lysiakova <volhalysiakova@gmail.com>
2021-07-26 17:03:16 +0300

    async

--- locators/async.js
+++ locators/async.js
@@ -18,13 +18,13 @@ it('Number of items - method WRAP', function () {
     cy.request('https://storage.googleapis.com/mannequin/2018/data/productwall/accessories/en_us.json?c=1571310916')
         .then((response) => {
 
-        cy.wrap(response.body).as("items");
+        cy.wrap(response.body).as("product");
 
     })
 
-    cy.get("@items").then(itemsList => {
-        cy.log("Total amount of products: " + itemsList.products.length)
-        cy.log(itemsList.products[0])
+    cy.get("@product").then(productList => {
+        cy.log("Total amount of products: " + productList.products.length)
+        cy.log(productList.products[0])
 
     })
 
